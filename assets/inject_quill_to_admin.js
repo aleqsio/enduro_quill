a173e43e27b7614d900614effff3c7c5c9a4bdbc
aleqsio <mikucki@gmail.com>
2018-01-05 20:27:42 +0100

    changed visible buttons

--- assets/inject_quill_to_admin.js
+++ assets/inject_quill_to_admin.js
@@ -9,9 +9,32 @@ enduro_admin_app.compileProvider
 		return {
 			link: function (scope, element, attr) {
 				$.getScript('https://cdn.quilljs.com/1.3.3/quill.js', function () {
+          console.log("injected to " + element);
+          var toolbarOptions = [
+            ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+            ['blockquote', 'code-block'],
+
+            [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+            [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+            [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+            [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+            [{ 'direction': 'rtl' }],                         // text direction
+
+            [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+            [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+            [ 'link', 'image', 'video', 'formula' ],
+            [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+            [{ 'font': [] }],
+            [{ 'align': [] }],
+
+            ['clean']                                         // remove formatting button
+          ];
 
 					var editor = new Quill(element[0], {
-						theme: 'snow'
+						theme: 'snow',
+            modules: {
+              toolbar: toolbarOptions
+            },
 					})
 
 					editor.on('text-change', function(delta, oldDelta, source) {
